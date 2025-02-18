ce1327b36478c29624a6705b0a335988b3d0e0df
Kenny1942 <wkenny.mohr@gmail.com>
2025-02-18 11:30:43 +0100

    first_commit

---
+++ main.dart
@@ -0,0 +1,15 @@
+void main() {
+  String gdStr = 'Guten';
+  String dayStr = 'Tag';
+  String worldStr = 'Welt';
+  int dd = 20;
+  String timeStreet = 'Zeit';
+  int seven = 7;
+  int twelve = 12;
+  String d = '!';
+  int a = 24;
+  String c = 'schöne';
+  String morning = 'Morgen';
+  String s = 'Heute';
+  String spa = '';
+}

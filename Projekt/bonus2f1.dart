2ad9744020f355235347f98fd2d3984b800f3ddd
Kenny1942 <wkenny.mohr@gmail.com>
2025-03-12 16:51:11 +0100

    bonusfunk 2commit

--- Projekt/bonus2f1.dart
+++ Projekt/bonus2f1.dart
@@ -1,36 +1,32 @@
 import 'dart:io';
 
-
 void main() {
+  void printGrades(List<int> grades) {
+    // Gibt alle Noten nacheinander aus
+    // z.B.: Note 1: 2, Note 2: 1, Note 3: 3
+    for (int i = 0; i < grades.length; i++) {
+      print('Note ${i + 1} :${grades[i]}');
+    }
+  }
+
+  void calculateAndPrintAverage(List<int> grades) {
+    // Berechnet den Durchschnitt und gibt aus:
+    // "Der Durchschnitt ist: 2.0"
+    double summe = 0;
+
+    for (int i = 0; i < grades.length; i++) {
+      summe = (summe + grades[i]);
+
+      //print(grades[i]);
+    }
+    double durchscnitt = summe / grades.length;
+    print('Durchschnitt: $durchscnitt');
+  }
+
   //Hauptfunktion
   void analyzeGrades(List<int> grades) {
-    
-    void printGrades(List<int> grades) {
-      // Gibt alle Noten nacheinander aus
-      // z.B.: Note 1: 2, Note 2: 1, Note 3: 3
-      for (int i=0; i<grades.length; i++ ){
-
-        print(grades[i]);
-      }
-    }
-
-    void calculateAndPrintAverage(List<int> grades) {
-      // Berechnet den Durchschnitt und gibt aus:
-      // "Der Durchschnitt ist: 2.0"
-      double summe=0;
-
-      for (int i=0; i<grades.length; i++ ){
-
-        summe= (summe+grades[i]);
-
-        print(grades[i]);
-      }
-              double durchscnitt =summe/grades.length;
-              print(durchscnitt);
-
-
-
-    }
+    printGrades(grades);
+    calculateAndPrintAverage(grades);
   }
 
   List<int> noten = [];
@@ -40,14 +36,14 @@ void main() {
 
   int amountNoten = int.parse(amountNotenString);
 
-  for (int i = 1; i < amountNoten; i++) {
+  for (int i = 1; i <= amountNoten; i++) {
     print('geben Sie Note $i ein:');
 
     String noteString = stdin.readLineSync() ?? '';
 
     int note = int.parse(noteString);
-    noten.add (note);
+    noten.add(note);
   }
 
-  analyzeGrades(List<int>  noten);
+  analyzeGrades(noten);
 }

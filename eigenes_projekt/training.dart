22cf460f41fc8b2396f8f3623a192cf9d4d6e8aa
Kenny1942 <wkenny.mohr@gmail.com>
2025-04-02 18:03:46 +0200

    2nd commit

--- eigenes_projekt/training.dart
+++ eigenes_projekt/training.dart
@@ -9,27 +9,29 @@ class Training {
   int beginTimeHour;
   int beginTimeMinute;
 
-  Training(
-      {required this.name,
-      required this.dateNumberDay,
-      required this.dateNumberMonth,
-      required this.dateYear,
-      required this.distanceMeters,
-      required this.durationHours,
-      required this.durationMinutes,
-      required this.beginTimeHour,
-      required this.beginTimeMinute});
+  Training({
+    required this.name,
+    required this.dateNumberDay,
+    required this.dateNumberMonth,
+    required this.dateYear,
+    required this.distanceMeters,
+    required this.durationHours,
+    required this.durationMinutes,
+    required this.beginTimeHour,
+    required this.beginTimeMinute,
+  });
 }
 
 void main() {
   Training t1 = Training(
-      name: 'run',
-      dateNumberDay: 18,
-      dateNumberMonth: 3,
-      dateYear: 2025,
-      distanceMeters: 50.8,
-      durationHours: 0,
-      durationMinutes: 45,
-      beginTimeHour: 16,
-      beginTimeMinute: 30);
+    name: 'run',
+    dateNumberDay: 18,
+    dateNumberMonth: 3,
+    dateYear: 2025,
+    distanceMeters: 50.8,
+    durationHours: 0,
+    durationMinutes: 45,
+    beginTimeHour: 16,
+    beginTimeMinute: 20,
+  );
 }
